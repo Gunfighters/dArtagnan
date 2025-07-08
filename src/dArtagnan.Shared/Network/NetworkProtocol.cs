981b469bfe20ced4c565d8d1c640466aa638607f
mssak <cxz017@naver.com>
2025-07-08 18:49:21 +0900

    id에따라 색깔을 부여받고 미니맵원은 accuracy에 따라 크기가 변하도록 변경

--- src/dArtagnan.Shared/Network/NetworkProtocol.cs
+++ src/dArtagnan.Shared/Network/NetworkProtocol.cs
@@ -25,7 +25,7 @@ namespace dArtagnan.Shared
     [Union(13, typeof(StartGame))]
     [Union(14, typeof(NewHost))]
     [Union(15, typeof(Winner))]
-    [Union(16, typeof(GamePlaying))]
+    //[Union(16, typeof(GamePlaying))]
     [Union(17, typeof(GameWaiting))]
     [Union(18, typeof(NewRound))]
     public interface IPacket
