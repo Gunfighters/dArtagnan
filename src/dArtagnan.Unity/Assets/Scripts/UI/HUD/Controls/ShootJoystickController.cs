1bc3c6302fb705e06216613375cc7cffd3e87713
TrulyBright <trulybright@yonsei.ac.kr>
2025-07-31 01:06:12 +0900

    ShootJoystickController.cs 간소화

--- src/dArtagnan.Unity/Assets/Scripts/UI/HUD/Controls/ShootJoystickController.cs
+++ src/dArtagnan.Unity/Assets/Scripts/UI/HUD/Controls/ShootJoystickController.cs
@@ -16,22 +16,18 @@ namespace UI.HUD.Controls
         public Image JoystickAxis;
         public Image HandleOutline;
         public Image Icon;
-        public PlayerCore LocalPlayerCore => PlayerGeneralManager.LocalPlayerCore;
+        private PlayerCore LocalPlayerCore => PlayerGeneralManager.LocalPlayerCore;
 
         private float RemainingReloadTime => LocalPlayerCore.Reload.RemainingReloadTime;
         private float TotalReloadTime => LocalPlayerCore.Reload.TotalReloadTime;
         private bool Shootable => RemainingReloadTime <= 0;
         private bool _reloading = true;
 
-        private readonly Color orange = new(1.0f, 0.64f, 0.0f);
+        private readonly Color _orange = new(1.0f, 0.64f, 0.0f);
         public Vector2 Direction => shootingJoystick.Direction;
     
         public bool Moving => Direction != Vector2.zero;
-
-        private bool _isPointerDown;
-        private Vector2 _lastDirection;
-        private bool hasAimed;
-
+        
         private void Update()
         {
             if (!LocalPlayerCore) return;
@@ -45,7 +41,7 @@ namespace UI.HUD.Controls
             }
             // shootButton.interactable = controlledPlayerCooldown <= 0;
             HandleOutline.color =
-                Icon.color = Shootable ? LocalPlayerCore.Shoot.Target is null ? orange : Color.red : Color.grey;
+                Icon.color = Shootable ? LocalPlayerCore.Shoot.Target is null ? _orange : Color.red : Color.grey;
             shootingJoystick.enabled = Shootable;
             cooldownImage.fillAmount = RemainingReloadTime <= 0 ? 1 : 1f - RemainingReloadTime / TotalReloadTime;
             if (_reloading && Shootable)
@@ -53,31 +49,19 @@ namespace UI.HUD.Controls
                 reloadSound.Play();
                 _reloading = false;
             }
-
-            if (_isPointerDown)
-            {
-                _lastDirection = Direction;
-            }
-
-            hasAimed |= Moving;
         }
 
         public void OnPointerDown(PointerEventData eventData)
         {
-            if (Shootable)
-            {
-                JoystickAxis.enabled = true;
-                _isPointerDown = true;
-            }
+            if (!Shootable) return;
+            JoystickAxis.enabled = true;
         }
     
         public void OnPointerUp(PointerEventData eventData)
         {
-            Debug.Log(_lastDirection);
             JoystickAxis.enabled = false;
-            _isPointerDown = false;
-            hasAimed = false;
-            if (Shootable && PlayerGeneralManager.LocalPlayerCore.Shoot.Target && (_lastDirection != Vector2.zero || _lastDirection == Vector2.zero && !hasAimed))
+            if (!Shootable) return;
+            if (PlayerGeneralManager.LocalPlayerCore.Shoot.Target)
                 PacketChannel.Raise(new PlayerShootingFromClient { TargetId = PlayerGeneralManager.LocalPlayerCore.Shoot.Target.ID });
         }
     }
