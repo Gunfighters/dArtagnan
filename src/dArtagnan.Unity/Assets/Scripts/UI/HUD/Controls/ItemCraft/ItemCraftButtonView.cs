8a7539a80078e33cd372a9b8822c5973ac88ab87
TrulyBright <trulybright@yonsei.ac.kr>
2025-08-07 15:00:59 +0900

    Complete #50 (아이템 사용 취소)

--- src/dArtagnan.Unity/Assets/Scripts/UI/HUD/Controls/ItemCraft/ItemCraftButtonView.cs
+++ src/dArtagnan.Unity/Assets/Scripts/UI/HUD/Controls/ItemCraft/ItemCraftButtonView.cs
@@ -20,8 +20,9 @@ namespace UI.HUD.Controls.ItemCraft
         [SerializeField] private GameObject descriptionBox;
         [SerializeField] private TextMeshProUGUI descriptionText;
         private InGameItem _item;
-        private bool hasItem;
         private bool canUseItem;
+        private bool hasItem;
+        private bool insideButton;
 
         private void Awake()
         {
@@ -42,6 +43,46 @@ namespace UI.HUD.Controls.ItemCraft
             filler.fillAmount = ratio;
         }
 
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            if (!hasItem)
+            {
+                PacketChannel.Raise(new UpdateItemCreatingStateFromClient { IsCreatingItem = true });
+                canUseItem = false;
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            transform.localScale = Vector3.one * 1.1f;
+            descriptionBox.SetActive(hasItem);
+            insideButton = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            transform.localScale = Vector3.one;
+            descriptionBox.SetActive(false);
+            insideButton = false;
+        }
+
+        public void OnPointerUp(PointerEventData eventData)
+        {
+            switch (hasItem)
+            {
+                case true when canUseItem && insideButton:
+                    PacketChannel.Raise(new UseItemFromClient());
+                    descriptionBox.SetActive(false);
+                    break;
+                case false:
+                    PacketChannel.Raise(new UpdateItemCreatingStateFromClient { IsCreatingItem = false });
+                    break;
+                default:
+                    canUseItem = hasItem;
+                    break;
+            }
+        }
+
         public void ShowItem(ItemId id)
         {
             hasItem = true;
@@ -59,44 +100,6 @@ namespace UI.HUD.Controls.ItemCraft
             hasItem = false;
             currentItemIcon.enabled = false;
             craftIcon.enabled = true;
-        }
-
-        public void OnPointerUp(PointerEventData eventData)
-        {
-            switch (hasItem)
-            {
-                case true when canUseItem:
-                    PacketChannel.Raise(new UseItemFromClient());
-                    descriptionBox.SetActive(false);
-                    break;
-                case false:
-                    PacketChannel.Raise(new UpdateItemCreatingStateFromClient { IsCreatingItem = false });
-                    break;
-                default:
-                    canUseItem = hasItem;
-                    break;
-            }
-        }
-
-        public void OnPointerDown(PointerEventData eventData)
-        {
-            if (!hasItem)
-            {
-                PacketChannel.Raise(new UpdateItemCreatingStateFromClient { IsCreatingItem = true });
-                canUseItem = false;
-            }
-        }
-
-        public void OnPointerEnter(PointerEventData eventData)
-        {
-            transform.localScale = Vector3.one * 1.1f;
-            descriptionBox.SetActive(hasItem);
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            transform.localScale = Vector3.one;
-            descriptionBox.SetActive(false);
         }
     }
 }
