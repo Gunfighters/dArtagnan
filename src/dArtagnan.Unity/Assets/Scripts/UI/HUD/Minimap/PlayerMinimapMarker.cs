72d1a08ff1347eeffeb1e68f41db3e62eb8f4675
TrulyBright <trulybright@yonsei.ac.kr>
2025-08-08 23:39:06 +0900

    feat: 미니맵 (#36)

---
+++ src/dArtagnan.Unity/Assets/Scripts/UI/HUD/Minimap/PlayerMinimapMarker.cs
@@ -0,0 +1,28 @@
+using Game;
+using Game.Player.Components;
+using UnityEngine;
+
+namespace UI.HUD.Minimap
+{
+    public class PlayerMinimapMarker : MonoBehaviour
+    {
+        public Sprite triangleSprite;
+        public Sprite circleSprite;
+        [SerializeField] private PlayerCore core;
+        private SpriteRenderer _spriteRenderer;
+
+        private void Awake()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void Update()
+        {
+            _spriteRenderer.sprite = core == PlayerGeneralManager.LocalPlayerCore ? triangleSprite : circleSprite;
+            _spriteRenderer.color = core.MyColor;
+            var t = Mathf.Clamp01(core.Accuracy.Accuracy.Value / 100f);
+            var scaleMultiplier = Mathf.Lerp(0.5f, 1.5f, t);
+            _spriteRenderer.transform.localScale = Vector3.one * scaleMultiplier;
+        }
+    }
+}
